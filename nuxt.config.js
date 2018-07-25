74d13c224b67283658c03b580987b7b6cf345ad7
skqksh <skqksh@naver.com>
2018-07-25 14:46:25 +0900

    express 와 body-parser를 이용하여, 로그인시 서버사이드에서 메시지를 받아 처리할 수 있도록 함

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const pkg = require('./package')
+const bodyParser = require('body-parser')
 
 module.exports = {
   mode: 'universal',
@@ -102,5 +103,10 @@ module.exports = {
 
   router: {
     middleware: 'log'
-  }
+  },
+
+  serverMiddleware: [
+    bodyParser.json(),
+    '~/api'
+  ]
 }
--- store/index.js
+++ store/index.js
@@ -105,6 +105,11 @@ const createStore = () => {
 
             Cookie.set('jwt', result.idToken);
             Cookie.set('expirationDate', expirationDate)
+
+            return this.$axios.$post('http://localhost:3000/api/track-data', {
+              data: 'Authenticated!'
+            })
+
           })
           .catch(e => console.log(e))
       },
